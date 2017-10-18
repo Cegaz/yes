a69365b6c53aeec785fac988426175bf3cf22609
Gilles Martinez <gilles.martinez33@gmail.com>
2017-10-18 10:17:25 +0200

    add mvc and layout

---
+++ app/dispatcher.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 17:20
+ */
+
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'Home/index');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        list($class, $method) = explode("/", $handler, 2);
+        $class = APP_CONTROLLER_NAMESPACE . $class;
+        echo call_user_func_array(array(new $class, $method), $vars);
+        break;
+}
---
+++ src/View/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="grid-x grid-margin-x">
+    <ul class="vertical menu small-6 medium-offset-4 medium-4 columns">
+        <li><a href="#" alt="histoire">Notre Histoire</a></li>
+        <li><a href="#" alt="partenaires">Nos partenaires</a></li>
+        <li><a href="#" alt="avis">Vous avez une idée ? Votre avis nous intéresse !</a></li>
+    </ul>
+    <ul class="vertical menu small-6 medium-4 columns">
+        <li><a href="#" alt="contact">Contact</a></li>
+        <li><a href="#" alt="mentions">Mentions légales</a></li>
+        <li><a href="#" alt="cgu">Conditions Générales d'Utilisation</a></li>
+    </ul>
+    </div>
+</footer>
+
+
+<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
+<script src="/assets/js/script.js"></script>
+</body>
+</html>
