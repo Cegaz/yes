6acc257fdf8fb61ec5c3f2829e025a443cb8d496
Hurricane <paradox-sales19@live.fr>
2017-10-25 13:53:02 +0200

    adding modal form

--- src/View/header.php
+++ src/View/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang="FR">
+<html class="no-js" lang="FR" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,9 +28,38 @@
         <div class="top-bar-right">
             <ul class="menu">
                 <li><input type="search" placeholder="Search"></li>
-                <li><button type="button" class="button">Connexion</button></li>
+                <li><a href="#openModal"><button type="button" class="button">Connexion</button></a></li>
+
+                <div id="openModal" class="modalDialog">
+                    <div>
+                        <a href="#close" title="Close" class="close">fermer</a>
+                        <div class="login-box">
+                            <div class="row collapse expanded">
+                                <div class="small-12 medium-6 column small-order-2 medium-order-1">
+                                    <div class="login-box-form-section">
+                                        <h1 class="login-box-title">Sign up</h1>
+                                        <form method="post" action="/signin">
+                                            <input class="login-box-input" type="text" required pattern="[a-zA-Z]+" name="pseudo"
+                                                   placeholder="pseudo" />
+                                            <input class="login-box-input" type="email" required pattern="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}" name="email"
+                                                   placeholder="email" />
+                                            <input class="login-box-input" type="password" id="password" required
+                                                   pattern="[a-zA-Z]+" name="password" placeholder="mot de passe" />
+
+                                            <input class="login-box-input" type="password" required pattern="[a-zA-Z]+"
+                                                   data-equalto="password" name="password_conf" placeholder="confirmer votre mot
+                                       de passe"/>
+                                            <input class="login-box-submit-button" type="submit" name="submit"
+                                                   value="valider" />
+                                            <input type="checkbox" name="cgu" value="1" placeholder="veuillez accepter les cgu" />
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
             </ul>
         </div>
     </div>
-
 </header>
