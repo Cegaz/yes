a58aa1f7d8edad8c136388ae194d1eec548586f2
Gilles <gilles.martinez33@gmail.com>
2017-11-08 10:43:19 +0100

    set routes for project holder only

---
+++ src/Model/ProjectHolderSpaceManager.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilles
+ * Date: 07/11/17
+ * Time: 17:36
+ */
+
+namespace Model;
+
+
+class ProjectHolderSpaceManager
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function checkProjectHolder($id) {
+        $queryHolder = "SELECT * 
+                        FROM user u
+                        INNER JOIN project_holder ph
+                        ON u.id = ph.id_user
+                        WHERE u.id = $id";
+        $result = $this->db->query($queryHolder);
+        $datas = $result->fetch(\PDO::FETCH_OBJ);
+        return $datas;
+    }
+
+}
