7fb61ebf898bee17ec0716d64b20112edb622f8f
Gilles <gilles.martinez33@gmail.com>
2017-11-08 17:54:43 +0100

    update ProjectsManager for getProjects request

--- src/Controller/ProjectHolderSpace.php
+++ src/Controller/ProjectHolderSpace.php
@@ -16,15 +16,17 @@ class ProjectHolderSpace extends AbstractController
     public function index() {
         require_once '../app/connect.php';
         $manager = new ProjectHolderSpaceManager($db);
+
         $datas = $manager->checkProjectHolder($_SESSION['idUser']);
         if($datas == false) {
             $unauthorized = "Vous n'avez pas accès à cet espace";
         } else {
             $_SESSION= $datas;
-            $projectPreparation = $manager->showProject($_SESSION->id, 'en rédaction');
-            $published = $manager->showProject($_SESSION->id, 'publié');
-            $financing = $manager->showProject($_SESSION->id, 'en financement');
-            $closed= $manager->showProject($_SESSION->id, 'terminé');
+            $projectManager = new ProjectsManager($db);
+            $projectPreparation = $projectManager->getProjectsAbstracts($_SESSION->id, 'en rédaction');
+            $published = $projectManager->getProjectsAbstracts($_SESSION->id, 'publié');
+            $financing = $projectManager->getProjectsAbstracts($_SESSION->id, 'en financement');
+            $closed= $projectManager->getProjectsAbstracts($_SESSION->id, 'terminé');
         }
             return $this->_twig->render('projectHolderSpace.html.twig', ['unauthorized' => $unauthorized, 'sess' => $_SESSION, 'projectPreparation' => $projectPreparation, 'published' => $published, 'financing' => $financing, 'closed' => $closed]);
 
