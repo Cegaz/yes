e32c454ca967bf87c5aefa50882bcaf5a8a2737f
Gilles <gilles.martinez33@gmail.com>
2017-11-16 21:55:29 +0100

    add styles to projectHolderSpace

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).foundation();
 
 $(document).ready(function() {
+    $('.holderSpaceTitle').parents('body').css("background", "url('/assets/img/bg.jpg')");
+    $('.holderSpaceTitle').parents('body').css("background-attachment", "fixed");
+    $('.holderSpaceTitle').parents('body').css("background-size", "cover");
     /* hide pour que ce soit masqué au départ */
     jQuery('#connectInputs').hide();
     /*ensuite je démarre onclick */
@@ -9,6 +12,7 @@ $(document).ready(function() {
         /* Vérification que l'action a été réalisée. SI c'est le cas, je transforme le bouton en OK */
         event.isPropagationStopped()
         $('#button').replaceWith('<button id="newButton" class="button" type="submit">OK !</button>');
+
     });
 
     /* au clic sur déconnexion redirige vers logout */
